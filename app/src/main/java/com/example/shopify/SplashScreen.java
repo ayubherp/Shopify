c1e6153ec5048d8cded7305f8eac156500d01418
ayubherp <ayubherp23@gmail.com>
2021-10-23 17:15:11 +0700

    E-Commerce Project UTS PBP

--- app/src/main/java/com/example/shopify/SplashScreen.java
+++ app/src/main/java/com/example/shopify/SplashScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.Window;
 
 import com.example.shopify.ui.auth.LoginActivity;
 
@@ -13,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_splash_screen);
         final Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
